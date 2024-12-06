44f003d7771f1110f8ae8ebdee10529948b1169d
Kabir Oberai <oberai.kabir@gmail.com>
2024-12-06 03:10:25 -0500

    this too

--- .github/workflows/build_release.sh
+++ .github/workflows/build_release.sh
@@ -4,5 +4,3 @@ bazel build --remote_header=x-buildbuddy-api-key="$BUILDBUDDY_API_KEY" //:for_al
 
 mv bazel-out/linux_arm64_musl-opt/bin/dist.tar.gz linux_arm64.tar.gz
 mv bazel-bin/linux_amd64_musl-opt/bin/dist.tar.gz linux_amd64.tar.gz
-
-shasum -a 256 *tar.gz > SHA256.txt
